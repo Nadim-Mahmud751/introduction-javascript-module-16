712bfa6d114cfbe13e4c8262fbafbeb95a3b260a
Nadim-Mahmud751 <mdnadimmahmud751@gmail.com>
2024-01-26 23:42:04 +0600

    end of the day poor

--- operation.js
+++ operation.js
@@ -11,3 +11,18 @@ console.log(10/3);
 // remainder
 console.log(10 % 3); //what will be the remainder after dividing 10 by 3
 console.log(11 % 7);
+
+
+// ----------------------------------
+var money = 560;
+money = 500;
+
+
+var price = 35;
+price = price + 10;
+console.log(price);
+
+var newPrice = price + 50;
+
+var discountedPrice = newPrice -10;
+console.log(discountedPrice);
