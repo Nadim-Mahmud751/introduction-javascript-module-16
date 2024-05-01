b52ae1f8b1d2a1f6305a2527e5b890c742a7604a
Nadim-Mahmud751 <mdnadimmahmud751@gmail.com>
2024-05-02 05:21:24 +0600

    add some old code

--- js-loops-module-18/range.js
+++ js-loops-module-18/range.js
@@ -1,14 +1,13 @@
 // let num = 1;
-// while (num <= 10){ //focus on the condition
+// while (num <= 10){  //focus on the condition
 //     console.log(num);
-//     num ++;
+//     num++;
 // }
 
 
 
-let num = 1;
+let num = 50;
 while (num <= 100){
     console.log(num);
-    num ++;
+    num++;
 }
-
